e67fc273fcdacc87195db19c1d4abeb2d525b7c6
Ivaylo Petrov <ivajloip@gmail.com>
2018-07-15 09:29:27 -0400

    Refactor code for sending requests

--- coapClient.py
+++ coapClient.py
@@ -7,41 +7,46 @@ from cbor2 import dumps, loads
 
 logging.basicConfig(level=logging.INFO)
 
-def set_req_params(request):
-    request.opt.uri_host=('example.com')
-    request.opt.uri_path=('c','RFy')
-    request.unresolved_remote = '[bbbb::2]:5683'
-    request.opt.content_format = 60
+COMMANDS = {
+   GET,
+   FETCH,
+   PUT
+}
+
+def set_req_params(request, *path):
+  request.opt.uri_host=('example.com')
+  request.opt.uri_path=('c',) + path
+  request.unresolved_remote = '[bbbb::2]:5683'
+  request.opt.content_format = 60
 
 async def make_req_and_parse_ans(protocol, request):
-    try:
-        response = await protocol.request(request).response
-    except Exception as e:
-        print('Failed to fetch resource:')
-        print(e)
-    else:
-        print('Result: %s\n%r'%(response.code, loads(response.payload)))
+  try:
+    response = await protocol.request(request).response
+  except Exception as e:
+    print('Failed to fetch resource:')
+    print(e)
+  else:
+    print('Result: %s\n%r'%(response.code, loads(response.payload)))
 
-async def send_get():
-    protocol = await Context.create_client_context()
+async def send(command, payload_object='', *path):
+  assert command in COMMANDS
 
-    request = Message(code=GET)
-    # request.set_request_uri(uri='coap://example.com/c/RFw', set_uri_host=False)
+  protocol = await Context.create_client_context()
 
-    set_req_params(request)
-    await make_req_and_parse_ans(protocol, request)
+  # Convert payload object to json (cbor?)
+  payload = _object_to_payload(payload_object)
 
-async def send_fetch():
-    protocol = await Context.create_client_context()
+  request = Message(code=command, payload=payload)
+  set_req_params(request, *path)
+  await make_req_and_parse_ans(protocol, request)
 
-    request = Message(code=FETCH, payload=b'\x82\x1A\x00\x01\x11\x73\x01')
-
-    set_req_params(request)
-    await make_req_and_parse_ans(protocol, request)
+def _object_to_payload(payload_object):
+  return payload_object
 
 async def main():
-    await send_fetch()
-    await send_get()
+  await send(GET, b'', "RFy")
+
+  await send(FETCH, b'\x82\x1A\x00\x01\x11\x73\x01')
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+  asyncio.get_event_loop().run_until_complete(main())
