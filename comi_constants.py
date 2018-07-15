55f30e95b01aa766b63981304e20f8bb6673b82d
Yousef Alowayed <alowayed@mit.edu>
2018-07-15 09:48:14 -0400

    bug importing ydk model

--- comi_constants.py
+++ comi_constants.py
@@ -1,6 +1,36 @@
+from ydk.providers import CodecServiceProvider
+from ydk.services import CodecService
+
+# Instantiate the codec service
+codec = CodecService()
+
+# Instantiate codec providers with json and xml options
+json_provider = CodecServiceProvider(type='json')
+
+# Declare the JSON configuration
+EXAMPLE_YANG_JSON = ''' {
+  "Cisco-IOS-XR-ifmgr-cfg:interface-configurations": {
+    "interface-configuration": [
+      {
+        "active": "act",
+        "interface-name": "Loopback0",
+        "description": "PRIMARY ROUTER LOOPBACK",
+        "Cisco-IOS-XR-ipv4-io-cfg:ipv4-network": {
+          "addresses": {
+            "primary": {
+              "address": "172.16.255.1",
+              "netmask": "255.255.255.255"
+            }
+          }
+        }
+      }
+    ]
+  }
+}
+'''
 
 # TODO: fill out
-EXAMPLE_YANG_OBJECT = None
+EXAMPLE_YANG_OBJECT = codec.decode(json_provider, EXAMPLE_YANG_JSON)
 
 # TODO: fix to fit our test case
 EXAMPLE_YANG_MAP = {
