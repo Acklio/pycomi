76d938665dc05879f98c0f621f21333c56c4ab48
Yousef Alowayed <alowayed@mit.edu>
2018-07-15 09:06:56 -0400

    start transform methods and tests

---
+++ comi_constants.py
@@ -0,0 +1,31 @@
+
+# TODO: fill out
+EXAMPLE_YANG_OBJECT = None
+
+# TODO: fix to fit our test case
+EXAMPLE_YANG_MAP = {
+  "Cisco-IOS-XR-ifmgr-cfg:interface-configurations": {
+    "interface-configuration": [
+      {
+        "active": "act",
+        "interface-name": "Loopback0",
+        "description": "PRIMARY ROUTER LOOPBACK",
+        "Cisco-IOS-XR-ipv4-io-cfg:ipv4-network": {
+          "addresses": {
+            "primary": {
+              "address": "172.16.255.1",
+              "netmask": "255.255.255.255"
+            }
+          }
+        }
+      }
+    ]
+  }
+}
+
+# TODO: implement
+EXAMPLE_CBOR_MAP_NO_DELTA = {
+
+}
+
+
---
+++ comi_utilities.py
@@ -0,0 +1,19 @@
+
+# TODO: implement
+def yang_object_to_map(yang_obj):
+    return yang_obj
+
+# TODO: implement
+def yang_map_to_cbor_map_no_delta(yang_map):
+    return yang_map
+
+# TODO: implement
+def cbor_map_no_delta_to_cbor_map_with_delta(cbor_map_no_delta):
+    return cbor_map_no_delta
+
+# TODO: implement
+def cbor_map_with_delta_to_cbor_object(cbor_map_with_delta):
+    return cbor_map_with_delta
+
+if __name__ == "__main__":
+    pass
