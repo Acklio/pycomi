a770586e4cc4bbac01b9de4d86af943e1a1cdf1a
Yousef Alowayed <alowayed@mit.edu>
2018-07-15 09:59:03 -0400

    working yang object

--- comi_constants.py
+++ comi_constants.py
@@ -1,36 +1,29 @@
 from ydk.providers import CodecServiceProvider
 from ydk.services import CodecService
+from ydk.models.cisco_ios_xr import Cisco_IOS_XR_ifmgr_cfg
 
 # Instantiate the codec service
 codec = CodecService()
 
-# Instantiate codec providers with json and xml options
-json_provider = CodecServiceProvider(type='json')
+# Instantiate the interface configuration class to configure the IPv4 loopback
+interface_configurations =  Cisco_IOS_XR_ifmgr_cfg.InterfaceConfigurations()
 
-# Declare the JSON configuration
-EXAMPLE_YANG_JSON = ''' {
-  "Cisco-IOS-XR-ifmgr-cfg:interface-configurations": {
-    "interface-configuration": [
-      {
-        "active": "act",
-        "interface-name": "Loopback0",
-        "description": "PRIMARY ROUTER LOOPBACK",
-        "Cisco-IOS-XR-ipv4-io-cfg:ipv4-network": {
-          "addresses": {
-            "primary": {
-              "address": "172.16.255.1",
-              "netmask": "255.255.255.255"
-            }
-          }
-        }
-      }
-    ]
-  }
-}
-'''
+# Instantiate the InterfaceConfiguration list instance
+interface_configuration = interface_configurations.InterfaceConfiguration()
+interface_configuration.active = "act"
+interface_configuration.interface_name = "Loopback0"
+interface_configuration.description = "PRIMARY ROUTER LOOPBACK"
+
+# Instantiate the Primary presence node
+interface_configuration.ipv4_network.addresses.primary = interface_configuration.ipv4_network.addresses.Primary()
+interface_configuration.ipv4_network.addresses.primary.address = "172.16.255.1"
+interface_configuration.ipv4_network.addresses.primary.netmask = "255.255.255.255"
+
+# Append the list instance to the parent list
+interface_configurations.interface_configuration.append(interface_configuration)
 
 # TODO: fill out
-EXAMPLE_YANG_OBJECT = codec.decode(json_provider, EXAMPLE_YANG_JSON)
+EXAMPLE_YANG_OBJECT = interface_configurations
 
 # TODO: fix to fit our test case
 EXAMPLE_YANG_MAP = {
